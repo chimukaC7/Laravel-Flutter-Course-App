fa1049bd62d9c1d14931c19c09c9928f32aebab4
Chimuka Moonde <chimukamoonde@gmail.com>
2024-04-26 12:05:48 +0200

    minor adjustments

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -5,6 +5,9 @@ class Category {
   Category({required this.id, required this.name});
 
   factory Category.fromJson(Map<String, dynamic> json) {
-    return Category(id: json['id'], name: json['name']);
+    return Category(
+        id: json['id'],
+        name: json['name']
+    );
   }
 }
