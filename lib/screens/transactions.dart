feabc9e0b4291272779489d16fa9e33668fa6546
Chimuka Moonde <chimukamoonde@gmail.com>
2024-08-18 15:47:57 +0200

    minor adjustments

--- lib/screens/transactions.dart
+++ lib/screens/transactions.dart
@@ -15,63 +15,88 @@ class _TransactionsState extends State<Transactions> {
   Widget build(BuildContext context) {
 
     final provider = Provider.of<TransactionProvider>(context);
-    List<Transaction> transactions = provider.transactions;
+    // List<Transaction> transactions = provider.transactions;
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Transactions'),
       ),
-      body: ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (context, index) {
-          Transaction transaction = transactions[index];
-          return ListTile(
-            title: Text('\$' + transaction.amount),
-            subtitle: Text(transaction.categoryName),
-            trailing: Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
-              Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-                Text(transaction.transactionDate),
-                Text(transaction.description),
-              ]),
-              IconButton(
-                icon: Icon(Icons.edit),
-                onPressed: () {
-                  showModalBottomSheet(
-                      isScrollControlled: true,
-                      context: context,
-                      builder: (BuildContext context) {
-                        return TransactionEdit(
-                            transaction, provider.updateTransaction);
-                      });
-                },
-              ),
-              IconButton(
-                icon: Icon(Icons.delete),
-                onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: Text("Confirmation"),
-                          content: Text("Are you sure you want to delete?"),
-                          actions: [
-                            TextButton(
-                              child: Text("Cancel"),
-                              onPressed: () => Navigator.pop(context),
-                            ),
-                            TextButton(
-                                child: Text("Delete"),
-                                onPressed: () => deleteTransaction(
-                                    provider.deleteTransaction, transaction, context)),
-                          ],
-                        );
-                      });
-                },
-              )
-            ]),
-          );
+      body: FutureBuilder<List<Transaction>>(
+        future: provider.apiService.fetchTransactions(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            // Show a loading spinner while the request is being processed
+            return const Center(child: CircularProgressIndicator());
+
+          } else if (snapshot.hasError) {
+            // Show an error message if something went wrong
+            return Center(child: Text('Error: ${snapshot.error}'));
+
+          }else if (snapshot.data!.isEmpty) {
+            // if that is not empty.
+            return const Center(child: Text('No items added yet.'));
+
+          } else if (snapshot.hasData) {
+            // Display the list of products if the data is available
+            final transactions = snapshot.data!;
+
+            return   ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (context, index) {
+                Transaction transaction = transactions[index];
+                return ListTile(
+                  title: Text('\$' + transaction.amount),
+                  subtitle: Text(transaction.categoryName),
+                  trailing: Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                    Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                      Text(transaction.transactionDate),
+                      Text(transaction.description),
+                    ]),
+                    IconButton(
+                      icon: Icon(Icons.edit),
+                      onPressed: () {
+                        showModalBottomSheet(
+                            isScrollControlled: true,
+                            context: context,
+                            builder: (BuildContext context) {
+                              return TransactionEdit(transaction, provider.updateTransaction);
+                            });
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text("Confirmation"),
+                                content: Text("Are you sure you want to delete?"),
+                                actions: [
+                                  TextButton(
+                                    child: Text("Cancel"),
+                                    onPressed: () => Navigator.pop(context),
+                                  ),
+                                  TextButton(
+                                      child: Text("Delete"),
+                                      onPressed: () => deleteTransaction(provider.deleteTransaction, transaction, context)),
+                                ],
+                              );
+                            });
+                      },
+                    )
+                  ]),
+                );
+              },
+            );
+          } else {
+            // Handle the case where no data was received
+            return Center(child: Text('No products available'));
+          }
         },
       ),
+
+
       floatingActionButton: new FloatingActionButton(
           onPressed: () {
             showModalBottomSheet(
